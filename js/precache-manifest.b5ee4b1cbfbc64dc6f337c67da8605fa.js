41f48a9b47b3e865ec1141b130e189f233789315
= <surajshindecse@gmail.com>
2020-02-29 02:54:02 +0530

    Updates

---
+++ js/precache-manifest.b5ee4b1cbfbc64dc6f337c67da8605fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72fc1fc16c2ecde1333f5a674c43200",
+    "url": "/js/index.html"
+  },
+  {
+    "revision": "e7902fe48bb04ac7427a",
+    "url": "/js/static/css/main.bbd35294.chunk.css"
+  },
+  {
+    "revision": "0914ea4f0b0cd1eb9f63",
+    "url": "/js/static/js/2.cb19d5d1.chunk.js"
+  },
+  {
+    "revision": "6a8e2fa5f8de2e25687af4e12c5e828c",
+    "url": "/js/static/js/2.cb19d5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7902fe48bb04ac7427a",
+    "url": "/js/static/js/main.9bb676a6.chunk.js"
+  },
+  {
+    "revision": "388c2554622df216b77d",
+    "url": "/js/static/js/runtime-main.7b80e186.js"
+  }
+]);
